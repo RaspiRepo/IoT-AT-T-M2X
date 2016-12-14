d767124f9d2a519662281284015f8f686d9c1d1e
RaspiRepo <RaspiRepo@users.noreply.github.com>
2016-12-14 11:52:11 -0800

    Update attm2m.py

--- attm2m.py
+++ attm2m.py
@@ -28,28 +28,42 @@ def getRAMinfo():
 
 #PI3 M2x
 cpu_temp        = 'cpu_temperature'
-primary_api_key = '91da0dbfec6930a7c1ee1659acdd3064'
-device_id       = 'a409729ce217060d60cee0525312d31c'
+cpu_usage       = 'cpu_usage'
+primary_api_key = 'primary_key from att m2x '
+device_id       = 'device id'
 
+cpu_load_threshold = 10 #10%
+cpu_temp_threshold = 2 #2 celsius
+
+#Create API access points
 client = M2XClient(primary_api_key)
+
+#stream identifier for each channel. Here CPU temperature and CPU load 
+#time series value uploaded to cloud.
 temp_stream = client.device(device_id).stream(cpu_temp)
-cpu_usage_stream = client.device(device_id).stream('cpu_usage')
+cpu_usage_stream = client.device(device_id).stream(cpu_usage)
 
 #result = stream.add_value(int(time.time()))
-count = 0
+count          = 0
 prv_temp       = 0
 prev_cpu_usage = 0
 
+#Infinite loop. Check every 10 seconds and if there is changes above specified threshold
+#update value to at&t m2x server
 while (count < 10) :
     CPU_temp = int(int(getCPUtemperature())/1000.0)
 
     curr_cpu_usage = float(getCPU_usage ())
 
-    if (abs(prv_temp - CPU_temp) > 2) :
+    #Send CPU temperature value only if there is a change from previous to current
+    #Threashold value > cpu_temp_threshold celsius
+    if (abs(prv_temp - CPU_temp) > cpu_temp_threshold) :
         temp_stream.add_value(CPU_temp)
         prv_temp = CPU_temp
 
-    if (abs(prev_cpu_usage - curr_cpu_usage) > 10) :
+    #Update CPU load only if considerable difference between previous and current
+    #
+    if (abs(prev_cpu_usage - curr_cpu_usage) > cpu_load_threshold) :
         #send values to m2m service
         cpu_usage_stream.add_value(curr_cpu_usage)
 
@@ -57,4 +71,3 @@ while (count < 10) :
 
     print("curr temp ", CPU_temp, "pretemp ", prv_temp, "cpu ", curr_cpu_usage, " prev ", prev_cpu_usage)
     time.sleep(10)
-    count += 0
